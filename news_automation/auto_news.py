74fd3b8f674d86502ce6b5b309a3b053e053e8a7
greatbrain <erickfania@gmail.com>
2023-02-07 16:45:30 -0400

    pdf extract file

---
+++ news_automation/auto_news.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+data = pd.read_csv("https://www.football-data.co.uk/mmz4281/2223/E0.csv")
+
+
+
+
+'''with open("data.csv", "w+") as file_csv:
+     file_csv.write(str(data))'''
