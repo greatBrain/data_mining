aba62e8503488f1ac506a175b6cece5c8bd837c4
greatbrain <erickfania@gmail.com>
2023-03-02 16:56:08 -0400

    Fix: Driver instance in main class correction.

--- news_automation/scrapper_copy.py
+++ news_automation/scrapper_copy.py
@@ -9,22 +9,24 @@ import pandas as pd
 class Driver:
       def __init__(self):
           self.driver = webdriver.Chrome(service=Service(manager().install()))
-          self.site_url = "https://www.thesun.co.uk/sport/football/"          
-      
-      def get_site_url(self):
-          return self.driver.get(self.site_url)
+          self.site_url = "https://www.thesun.co.uk/sport/football/"
 
 
 class Main:
       def __init__(self):
-     	    #Create the Driver class instance:
-          self.web_driver = Driver()
+     	  #Create the Driver class instance:
+     	  self.driver_instance = Driver()
+     	  self.web_driver = self.driver_instance.driver
+     	  self.web_driver.get(self.driver_instance.site_url)
 
       def get_all_data(self):
-          elements_container =  self.web_driver.driver.find_elements(by='xpath', value='//div[@class="teaser__copy-container"]')
-          return elements_container.text
+          elements_container = self.web_driver.find_elements(by="xpath", value='//div[@class="teaser__copy-container"]')
+          return elements_container
 
-      
-# main_class = Main()
-# print(main_class.get_all_data())
-      
+      def print_data(self):
+          subtitle = [data.find_element(by="xpath", value="./a/p").text for data in self.get_all_data()]
+          return subtitle
+
+if __name__ == '__main__':
+   main_class = Main()
+   print(main_class.print_data())
