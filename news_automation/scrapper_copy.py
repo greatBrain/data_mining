7c591bbeeb155c71476555bddb80e2768296f251
greatbrain <erickfania@gmail.com>
2023-03-04 08:43:21 -0400

    New: exporting data to a csv file.

--- news_automation/scrapper_copy.py
+++ news_automation/scrapper_copy.py
@@ -1,5 +1,5 @@
 #Copy of original web scrapper file.
-
+import os
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager as manager
@@ -13,20 +13,45 @@ class Driver:
 
 
 class Main:
-      def __init__(self):
-     	  #Create the Driver class instance:
-     	  self.driver_instance = Driver()
-     	  self.web_driver = self.driver_instance.driver
-     	  self.web_driver.get(self.driver_instance.site_url)
+      def __init__(self):     	  
+          self.driver_instance = Driver()
+          self.web_driver = self.driver_instance.driver
+          self.web_driver.get(self.driver_instance.site_url)          
 
+      #Get all data from the web:   
       def get_all_data(self):
           elements_container = self.web_driver.find_elements(by="xpath", value='//div[@class="teaser__copy-container"]')
           return elements_container
 
-      def print_data(self):
-          subtitle = [data.find_element(by="xpath", value="./a/p").text for data in self.get_all_data()]
-          return subtitle
+      #Search inside all data element by element:
+      def get_elements(self):
+          try:
+             #title = [data.find_element(by="xpath", value="./a/h2").text for data in self.get_all_data()]
+             subtitle = [data.find_element(by="xpath", value="./a/p").text for data in self.get_all_data()]
+             links = [data.find_element(by="xpath", value="./a").get_attribute("href") for data in self.get_all_data()]
+
+             data = {"subtitles":subtitle, "links":links}
+             return data
+          
+          except Exception as e:
+                 return False          
+
+
+      def export_data(self):
+          
+          if self.get_elements() !=False:
+             dataframe = pd.DataFrame(self.get_elements())       
+
+             while True:            
+                if not os.path.isdir('data_files'):        
+                   os.mkdir('data_files')                                       
+                dataframe.to_csv('data_files/data.csv')
+                break                
+
+          else:
+             print("An exception has ocurred, the operation could not be completed. Try again.\n")
+
 
 if __name__ == '__main__':
    main_class = Main()
-   print(main_class.print_data())
+   main_class.export_data()
