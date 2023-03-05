9ed31f17daf3d20ddedf2284dfcb33c3b71769e1
greatbrain <erickfania@gmail.com>
2023-03-05 11:10:44 -0400

    ADD: New class Scraper to handle all operations, separate of the Main class.

--- news_automation/scrapper_copy.py
+++ news_automation/scrapper_copy.py
@@ -1,22 +1,32 @@
 #Copy of original web scrapper file.
 import os
+import sys
+from datetime import datetime
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager as manager
 import pandas as pd
 
 
 class Driver:
       def __init__(self):
-          self.driver = webdriver.Chrome(service=Service(manager().install()))
+          #For headless mode:   	 
+          options = Options()
+          options.headless = True 
+          self.driver = webdriver.Chrome(service=Service(manager().install()), options=options)
           self.site_url = "https://www.thesun.co.uk/sport/football/"
 
 
-class Main:
-      def __init__(self):     	  
+          
+class Scraper():
+      def __init__(self):                 
           self.driver_instance = Driver()
           self.web_driver = self.driver_instance.driver
-          self.web_driver.get(self.driver_instance.site_url)          
+          self.web_driver.get(self.driver_instance.site_url)    
+          print("Page URL:", self.web_driver.current_url)          
+          print("Page Title:", self.web_driver.title)
+      
 
       #Get all data from the web:   
       def get_all_data(self):
@@ -39,19 +49,41 @@ class Main:
 
       def export_data(self):
           
-          if self.get_elements() !=False:
-             dataframe = pd.DataFrame(self.get_elements())       
+          try:
+          
+            dataframe = pd.DataFrame(self.get_elements()) 
 
-             while True:            
-                if not os.path.isdir('data_files'):        
-                   os.mkdir('data_files')                                       
-                dataframe.to_csv('data_files/data.csv')
-                break                
+            #Get the executable app path:
+            app_path = os.path.dirname(sys.executable)
+            
+            #Get current date and time to name the file:
+            now = datetime.now()
+            current_date = now.strftime("%d%m%Y")
+            file_name = f'data_file_{current_date}.csv'  #Name news files as the current day
 
-          else:
-             print("An exception has ocurred, the operation could not be completed. Try again.\n")
+            #Save the new created file to the path, this is a better way to save the files no metter the OS used:
+            file_path = os.path.join(app_path, file_name)          
+            dataframe.to_csv(file_path)  
 
+            self.driver_instance.quit()      
+
+          except Exception as e:
+              print(f"Error, the operation could not be completed: ", e, "try again.\n")
+              self.web_driver.quit()   
+
+
+      def run(self):
+          return self.export_data() 
+      
+
+class Main:
+      def __init__(self):                 
+          self.scrpaer = Scraper()
+
+      def main(self) -> None:
+          self.scrpaer.run()        
+              
 
 if __name__ == '__main__':
    main_class = Main()
-   main_class.export_data()
+   main_class.main()
